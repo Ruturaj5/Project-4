76b80d27c94005b10216f339cf7fe19ba867a1ad
rutur <rutur@RUTURAJ>
2024-01-11 16:09:21 +0530

    By Ruturaj

---
+++ Git Program 4th Project 11 January/src/com/cjc/Demo4/Git_Demo4.java
@@ -0,0 +1,18 @@
+package com.cjc.Demo4;
+
+public class Git_Demo4 
+{
+    public void m4() 
+    {
+    	System.out.println("Method m4");
+    }
+	public static void main(String[] args) 
+	{
+	
+		System.out.println("Main method");
+		Git_Demo4 g4 = new Git_Demo4 ();
+		g4.m4();
+		
+	}
+
+}
